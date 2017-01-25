80c2c54f37f687274bbc91a6effcde4cb5376c4b
Osmond Oscar <osmond.oranagwa@andela.com>
2017-01-25 11:23:32 +0100

    remove override for #reload

--- lib/instance_methods.rb
+++ lib/instance_methods.rb
@@ -4,25 +4,25 @@ module ObfuscateId
       ObfuscateId.hide(self.id, self.class.obfuscate_id_spin)
     end
 
-    # Override ActiveRecord::Persistence#reload
-    # passing in an options flag with { no_obfuscated_id: true }
-    def reload(options = nil)
-      options = (options || {}).merge(no_obfuscated_id: true)
-
-      clear_aggregation_cache
-      clear_association_cache
-
-      fresh_object =
-        if options && options[:lock]
-          self.class.unscoped { self.class.lock(options[:lock]).find(id, options) }
-        else
-          self.class.unscoped { self.class.find(id, options) }
-        end
-    
-      @attributes = fresh_object.instance_variable_get('@attributes')
-      @new_record = false
-      self
-    end
+    # # Override ActiveRecord::Persistence#reload
+    # # passing in an options flag with { no_obfuscated_id: true }
+    # def reload(options = nil)
+    #   options = (options || {}).merge(no_obfuscated_id: true)
+    #
+    #   clear_aggregation_cache
+    #   clear_association_cache
+    #
+    #   fresh_object =
+    #     if options && options[:lock]
+    #       self.class.unscoped { self.class.lock(options[:lock]).find(id, options) }
+    #     else
+    #       self.class.unscoped { self.class.find(id, options) }
+    #     end
+    #
+    #   @attributes = fresh_object.instance_variable_get('@attributes')
+    #   @new_record = false
+    #   self
+    # end
 
     def deobfuscate_id(obfuscated_id)
       self.class.deobfuscate_id(obfuscated_id)
